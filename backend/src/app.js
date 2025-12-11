a97a97f35853cce3d6fa93849a7ffc36365c8625
Ank4031 <bluepearl4031@gmail.com>
2025-12-11 10:19:20 +0530

    Update CORS origin to allow requests from the new deployment URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;  
 app.use(
   cors({
-    origin: "https://ai-chat-app-9.onrender.com",
+    origin: "https://ai-chat-app-13.onrender.com",
     credentials: true,
   })
 );
